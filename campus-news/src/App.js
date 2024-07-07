0008ab19ebc3090c96eca302357bdf6f77044edb
Aditya <f20220497@goa.bits-pilani.ac.in>
2024-07-07 14:29:36 +0530

    register page complete

--- campus-news/src/App.js
+++ campus-news/src/App.js
@@ -1,15 +1,12 @@
 import './App.css';
-import Login from './pages/login/login';
+// import Login from './pages/login/login';
 // import Home from './pages/home/home';
 // import Profile from './pages/profile/profile';
-// import Register from './pages/register/register';
+import Register from './pages/register/register';
 function App() {
   return (
     <div>
-      <Login/>
-      {/* <Register/>
-      <Home/>
-      <Profile/> */}
+      <Register/>
     </div>
   );
 }
--- campus-news/src/pages/register/register.jsx
+++ campus-news/src/pages/register/register.jsx
@@ -3,7 +3,26 @@ import './register.scss'
 const Register = () => {
   return (
     <div className="register">
-      register works!
+      <div className="card">
+        <div className="left">
+            <h1>BPGC News Portal</h1>
+            <p>
+                The news portal of BITS Goa, social media application.
+            </p>
+            <span>Already have an account?</span>
+            <button>Login</button>
+        </div>
+        <div className="right">
+            <h1>Register</h1>
+            <form>
+                <input type="text" placeholder='Name'></input>
+                <input type="text" placeholder='Username'></input>
+                <input type="email" placeholder='Email'></input>
+                <input type="password" placeholder='Password'></input>
+                <button>Register</button>
+            </form>
+        </div>
+      </div>
     </div>
   )
 }

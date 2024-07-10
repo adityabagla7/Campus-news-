c86d19782e66c243faec17de436f5265c758b3ac
Aditya <f20220497@goa.bits-pilani.ac.in>
2024-07-10 18:26:04 +0530

    database setup

---
+++ Api/index.js
@@ -0,0 +1,7 @@
+import Express from "express";
+import userRoutes from "./routes/users.js"
+const app = Express();
+app.use("/api/users", userRoutes);
+app.listen(8800, ()=>{
+    console.log("Api working....");
+})
